0f03f766cf4840d090c652010d37eeee6a64ec03
mahabub5090 <mdmahabub.rahman789@gmail.com>
2024-07-02 23:50:09 +0600

    Day_2_July_2024

---
+++ 350. Intersection of Two Arrays II.cpp
@@ -0,0 +1,13 @@
+// Problem Link: https://leetcode.com/problems/intersection-of-two-arrays-ii/description/?envType=daily-question&envId=2024-07-02
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        map<int,int>mp; for(auto &c:nums1)mp[c]++;
+        for(auto &c:nums2){
+            if(mp[c])mp[c]--,ans.push_back(c);
+        }
+        return ans;
+    }
+};
