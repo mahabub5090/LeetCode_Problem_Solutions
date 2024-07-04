da9768f9c7afde2b8b978c3c1f94f1116bced374
mahabub5090 <mdmahabub.rahman789@gmail.com>
2024-07-04 23:26:47 +0600

    Day_4_July_2024

---
+++ 2181. Merge Nodes in Between Zeros.cpp
@@ -0,0 +1,42 @@
+// Problem Link: https://leetcode.com/problems/merge-nodes-in-between-zeros/?envType=daily-question&envId=2024-07-04
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        vector<int>s;
+        while(head->next!=NULL){
+            s.push_back(head->val);
+            head=head->next;
+        }
+        vector<int>ans;
+        int i=s.size(),sum=0;
+        while(--i>=0){
+            while(s[i]!=0){
+                sum+=s[i--];
+            }
+            ans.push_back((sum));
+            sum=0;
+        }
+        reverse(ans.begin(),ans.end());
+        ListNode *head2=new ListNode(),*tail=head2,*prev;
+        i=0;
+        while(i<ans.size()){
+            tail->val=ans[i++];
+            tail->next=new ListNode();
+            prev=tail;
+            tail=tail->next;
+        }
+        prev->next=NULL;
+        return head2;     
+    }
+};
