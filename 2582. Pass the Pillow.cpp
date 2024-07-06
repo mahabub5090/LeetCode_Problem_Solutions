55e950a9e2390a9682fefc04625f3e257c7c0814
mahabub5090 <mdmahabub.rahman789@gmail.com>
2024-07-06 21:51:21 +0600

    Day_6_July_2024

---
+++ 2582. Pass the Pillow.cpp
@@ -0,0 +1,10 @@
+// Problem Link: https://leetcode.com/problems/pass-the-pillow/description/?envType=daily-question&envId=2024-07-06
+
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        // if(time<n)return time+1;
+        int way=(time/(n-1))%2;
+        return !way? (time%(n-1))+1: n-(time%(n-1));
+    }
+};
