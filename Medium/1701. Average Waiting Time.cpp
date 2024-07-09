71ea1ead2b53cce7aa9561f7d20f5cdf49d4e8d7
mahabub5090 <mdmahabub.rahman789@gmail.com>
2024-07-09 19:27:11 +0600

    Day_9_July_2024

---
+++ Medium/1701. Average Waiting Time.cpp
@@ -0,0 +1,13 @@
+// Problem Link: https://leetcode.com/problems/average-waiting-time/description/?envType=daily-question&envId=2024-07-09
+
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        long long cnt=0,limit=customers[0][0];
+        for(auto &c:customers){
+            if(limit>=c[0])limit+=c[1],cnt+=limit-c[0];
+            else limit=c[0]+c[1],cnt+=c[1];            
+        }
+        return cnt*1.0/customers.size();
+    }
+};
