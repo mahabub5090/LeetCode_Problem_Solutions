3e4c2b43ee09accdd66a29d7fb02232e52c997f4
mahabub5090 <mdmahabub.rahman789@gmail.com>
2024-07-12 18:21:15 +0600

    Most optimize solution of '1190. Reverse Substrings Between Each Pair of Parentheses' added

--- Medium/1190. Reverse Substrings Between Each Pair of Parentheses.cpp
+++ Medium/1190. Reverse Substrings Between Each Pair of Parentheses.cpp
@@ -31,3 +31,37 @@ public:
 
 // Time Complexity => O(N^2)
 // Space Complexity => O(1)
+//==========================;
+// way-2 (Most optimize solution) =>
+
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        int n=s.size();
+        vector<int>pair(n,-1);
+        stack<int>open;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(')open.push(i);
+            else if(s[i]==')'){
+                pair[open.top()]=i;
+                pair[i]=open.top();
+                open.pop();
+            }
+        }
+        string ans="";
+        for(int i=0,dir=1;i<n;i+=dir){
+            if(s[i]=='(' || s[i]==')'){
+                i=pair[i];
+                dir*=-1;
+            }
+            else{
+                ans+=s[i];
+            }
+        }
+        return ans;
+
+    }
+};
+
+// Time Complexity => O(2*N)
+// Space Complexity => O(2*N)
