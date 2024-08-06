a130c9ca01770eefec28bd91822e18bdbcbce058
mahabub5090 <mdmahabub.rahman789@gmail.com>
2024-08-06 21:06:02 +0600

    Day_6_August_2024

---
+++ Medium/3016. Minimum Number of Pushes to Type Word II.cpp
@@ -0,0 +1,20 @@
+// Problem Link: https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/description/?envType=daily-question&envId=2024-08-06
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int>vc(26);
+        for(auto &c:word)vc[c-'a']++;
+        sort(vc.begin(),vc.end(),[](int a,int b){
+            return a>b;
+        });
+        int ans=0,cnt=0;
+        for(auto &c:vc){
+            ans+=c*(1+(cnt++/8));
+        }
+        return ans;   
+    }
+};
+
+// Time Complexity : O(2*N+NlogN) => O(NlogN);
+// Space Complexity: O(N);
