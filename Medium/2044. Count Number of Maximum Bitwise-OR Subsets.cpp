96bcd568593ebd6eeeef738c5975c6d2e0fa1717
mahabub5090 <mdmahabub.rahman789@gmail.com>
2024-10-19 19:52:43 +0600

    Yesterday's LeetCode daily problems solution added

--- Medium/2044. Count Number of Maximum Bitwise-OR Subsets.cpp
+++ Medium/2044. Count Number of Maximum Bitwise-OR Subsets.cpp
@@ -1,2 +1,21 @@
 // Problem Link: https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/description/?envType=daily-question&envId=2024-10-18
 
+class Solution {
+public:
+    void dfs(int i,auto &vc,int cor,int mor,int &cnt){
+        if(i==vc.size())return;
+        if((cor|vc[i])==mor)cnt++;
+        dfs(i+1,vc,cor|vc[i],mor,cnt);
+        dfs(i+1,vc,cor,mor,cnt);
+    }
+    int countMaxOrSubsets(vector<int>& nums) {
+        int mor=0;
+        for(auto &c:nums)mor|=c;
+        int cnt=0;
+        dfs(0,nums,0,mor,cnt);
+        return cnt;
+    }
+};
+
+// Time Complexity : O(2^N);
+// Space Complexity: O(N); // for call stack;
