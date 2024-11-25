48da98c27546d11acdf83ddc9d2ca2895de8fd68
mahabub5090 <mdmahabub.rahman789@gmail.com>
2024-11-25 21:09:39 +0600

    Day_25_November_2024

---
+++ Medium/2924. Find Champion II.cpp
@@ -0,0 +1,20 @@
+// Problem Link: https://leetcode.com/problems/find-champion-ii/submissions/1463304985/?envType=daily-question&envId=2024-11-26
+
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int>parent(n,-1);
+        for(auto &c:edges){
+            parent[c[1]]=c[0];
+        }
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(parent[i]==-1)ans.push_back(i);
+        }
+        if(ans.size()>1)return -1;
+        return ans.back();
+    }
+};
+
+// Time Complexity : O(2*N) => O(N);
+// Space Complexity: O(2*N) => O(N);
