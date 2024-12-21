276f64ec5d3efeb102c8d2cf37229cdc926e7769
mahabub5090 <mdmahabub.rahman789@gmail.com>
2024-12-21 21:04:53 +0600

    Day_21_December_2024

---
+++ Hard/2872. Maximum Number of K-Divisible Components.cpp
@@ -0,0 +1,30 @@
+// Problem Link: https://leetcode.com/problems/maximum-number-of-k-divisible-components/description/?envType=daily-question&envId=2024-12-21
+
+class Solution {
+public:
+    int ans=0;
+    unordered_map<int,vector<int>>adj;
+    long long dfs(int node,int parent,int &k,auto &values){
+        long long subTreeSum=values[node];
+        for(auto &c:adj[node]){
+            if(c!=parent){
+                subTreeSum+=dfs(c,node,k,values);
+            }
+        }
+        if(subTreeSum%k==0)ans++,subTreeSum=0;
+        return subTreeSum;
+    }
+
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        for(auto &c:edges){
+            adj[c[0]].push_back(c[1]);
+            adj[c[1]].push_back(c[0]);
+        }
+        dfs(0,-1,k,values);
+        return ans;
+    }
+};
+
+
+// Time Complexity : O(N);
+// Space Complexity: O(N);
