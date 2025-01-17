0c2a660da8695f9c8751bf2ad745c9595658b770
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-01-17 23:39:07 +0600

    Solution updated of the problem '2683. Neighboring Bitwise XOR'

--- Medium/2683. Neighboring Bitwise XOR.cpp
+++ Medium/2683. Neighboring Bitwise XOR.cpp
@@ -3,6 +3,29 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
+        int n=derived.size();
+        int xr=0,last=0;
+        for(int i=0;i<n;i++){
+            last=last^derived[i];
+        }
+        if(0==last)return 1;
+        xr=1,last=1;
+        for(int i=0;i<n;i++){
+            last=last^derived[i];
+        }
+        if(1==last)return 1;
+        return 0;
+    }
+};
+
+// Time Complexity : O(N);
+// Space Complexity: O(1);
+
+// Way: 2 =>
+
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
         int xr=0;
         for(auto &c:derived)xr^=c;
         return xr==0;
