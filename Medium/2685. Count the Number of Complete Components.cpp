e0702885bf81f8ec01229e8ee5f4bc61a3e4bbf0
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-03-22 23:51:30 +0600

    Day_22_March_2025

---
+++ Medium/2685. Count the Number of Complete Components.cpp
@@ -0,0 +1,37 @@
+// Problem Link: https://leetcode.com/problems/count-the-number-of-complete-components/description/?envType=daily-question&envId=2025-03-22
+
+class Solution {
+public:
+    void dfs(int i,auto &graph,auto &vis,auto &comp,int &edges){
+        vis[i]=1;
+        comp.push_back(i);
+        edges+=graph[i].size();
+
+        for(auto &c:graph[i]){
+            if(vis[c]==0)dfs(c,graph,vis,comp,edges);
+        }
+    }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>>graph(n);
+        for(auto &c:edges){
+            graph[c[0]].push_back(c[1]);
+            graph[c[1]].push_back(c[0]);
+        }
+        int ans=0;
+        vector<int>vis(n,0);
+        for(int i=0;i<n;i++){
+            if(vis[i])continue;
+
+            int edges=0;
+            vector<int>components;
+            dfs(i,graph,vis,components,edges);
+
+            int nodeCount=components.size();
+            if(nodeCount*(nodeCount-1)==edges)ans++;
+        }
+        return ans;
+    }
+};
+
+// Time Complexity : O(N);
+// Space Complexity: O(N);
