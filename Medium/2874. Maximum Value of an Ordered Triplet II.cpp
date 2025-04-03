ba41493ecdea2dcec2e29ffd4936ed4b517ab102
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-04-03 21:48:20 +0600

    Day_03_April_2025

---
+++ Medium/2874. Maximum Value of an Ordered Triplet II.cpp
@@ -0,0 +1,22 @@
+// Problem Link: https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/description/?envType=daily-question&envId=2025-04-03
+
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+    int n=nums.size();
+      vector<int>lm(n,0),rm(n,0);
+      for(int i=1;i<n;i++){
+        lm[i]=max(lm[i-1],nums[i-1]);
+        rm[n-i-1]=max(rm[n-i],nums[n-i]);
+      }
+
+      long long ans=0;
+      for(int i=1;i<n-1;i++){
+        ans=max(ans,(long long)(lm[i]-nums[i])*rm[i]);
+      }
+      return ans;
+    }
+};
+
+// Time Complexity : O(2*N) => O(N);
+// Space Complexity: O(2*N) => O(N);
