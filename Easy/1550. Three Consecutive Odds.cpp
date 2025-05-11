2fdd5101b0ea0f3f741f2b131298de4b77b8b850
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-05-11 19:24:54 +0600

    Day_11_May_2025

--- Easy/1550. Three Consecutive Odds.cpp
+++ Easy/1550. Three Consecutive Odds.cpp
@@ -15,3 +15,18 @@ public:
 
 // Time Complexity : O(N);
 // Space Complexity: O(1);
+
+// Way: 2 =>
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        for(int i=2;i<arr.size();i++){
+            if(arr[i]%2 && arr[i-1]%2 && arr[i-2]%2)return 1;
+        }
+        return 0;
+    }
+};
+
+// Time Complexity : O(N);
+// Space Complexity: O(1);
