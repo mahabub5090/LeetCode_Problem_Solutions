b4e524d6393627d21ab585debc80904849c6b654
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-06-06 21:46:04 +0600

    Solution of the problem '909. Snakes and Ladders' was updated

--- Medium/909. Snakes and Ladders.cpp
+++ Medium/909. Snakes and Ladders.cpp
@@ -2,36 +2,41 @@
 
 class Solution {
 public:
+    int ans=INT_MAX;
+    map<int,pair<int,int>>mp;
+    unordered_map<int,int>dp;
+    
+    void makeMap(int n){
+        int num=1;
+        for(int i=n-1;i>=0;i--){
+            int l=n-i;
+            for(int j=(l%2?0:n-1);j!=(l%2?n:-1);j+=(l%2?1:-1))mp[num++]={i,j};
+        }
+    }
+
+    void getAns(int num,int cnt,auto &board){        
+        if(num==mp.size()){
+            ans=min(ans,cnt);
+            return;
+        }
+
+        if(dp.count(num) && dp[num]<=cnt)return;
+        dp[num]=cnt;
+
+        for(int step=num+1;step<=min(num+6,(int)mp.size());step++){
+            auto [ni,nj]=mp[step];
+            if(board[ni][nj]!=-1){
+                getAns(board[ni][nj],cnt+1,board);
+            }
+            else getAns(step,cnt+1,board);
+        }
+    }
+
     int snakesAndLadders(vector<vector<int>>& board) {
-        int n=board.size(),label=1;
-        vector<pair<int,int>>cells(n*n+1);
-        vector<int>cols(n);
-        iota(cols.begin(),cols.end(),0);
-
-        for(int r=n-1;r>=0;r--) {
-            for(int c:cols) {
-                cells[label++]={r,c};
-            }
-            reverse(cols.begin(),cols.end());  
-        }
-
-        vector<int>dist(n*n+1,-1);
-        dist[1]=0;
-        queue<int>q;
-        q.push(1);
-
-        while(q.size()) {
-            int curr=q.front();q.pop();
-            for(int i=1;i<=6 && curr+i<=n*n;i++) {
-                auto [r,c]=cells[curr+i];
-                int dest=board[r][c]==-1?curr+i:board[r][c];
-                if(dist[dest]==-1) {
-                    dist[dest]=dist[curr]+1;
-                    q.push(dest);
-                }
-            }
-        }
-        return dist[n*n];
+        int n=board.size();
+        makeMap(n);
+        getAns(1,0,board);
+        return ans>n*n?-1:ans;  
     }
 };
 
