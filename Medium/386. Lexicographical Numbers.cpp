edcae127a47abb75646043165bd07c85fc86b538
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-06-08 23:43:05 +0600

    Day_08_June_2025

--- Medium/386. Lexicographical Numbers.cpp
+++ Medium/386. Lexicographical Numbers.cpp
@@ -7,11 +7,13 @@ public:
         if(c>n)return;
         ans.push_back(c);
         c*=10;
-        for(int i=0;i<=9;i++)dfs(c+i,ans,n);
+        for(int i=0;i<10;i++){
+            dfs(c+i,ans,n);
+        }
     }
     vector<int> lexicalOrder(int n) {
         vector<int>ans;
-        for(int i=1;i<=9;i++){
+        for(int i=1;i<10;i++){
             dfs(i,ans,n);
         }
         return ans;
