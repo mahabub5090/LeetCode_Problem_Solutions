c10cd4a1a385b5f7ddaaff8ebcdc029f361ff386
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-06-29 23:57:34 +0600

    Day_29_June_2025

--- Hard/2014. Longest Subsequence Repeated k Times.cpp
+++ Hard/2014. Longest Subsequence Repeated k Times.cpp
@@ -43,4 +43,4 @@ public:
 };
 
 // Time Complexity : O(N*(N\K)!);
-// Space Complexity: O(N/K)!
+// Space Complexity: O(N/K)!;
---
+++ Medium/1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -0,0 +1,1 @@
+// Problem Link: https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/description/?envType=daily-question&envId=2025-06-29 .
