c973173b787e847b8b067b5f4c6587c41b206be6
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-06-29 23:40:53 +0600

    Daily problem of 'Day_29_June_2025' solution added

--- Medium/1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
+++ Medium/1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -1,1 +1,26 @@
 // Problem Link: https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/description/?envType=daily-question&envId=2025-06-29 .
+
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        int n=nums.size(),mod=1e9+7;
+        sort(nums.begin(),nums.end());
+
+        vector<long long>pw(n);pw[0]=1;
+        for(int i=1;i<n;i++)pw[i]=2*pw[i-1],pw[i]%=mod;
+
+        long long ans=0,i=0,j=n-1;
+        while(i<=j){
+            if(nums[i]+nums[j]<=target){
+                ans+=pw[j-i];
+                ans%=mod;
+                i++;
+            }
+            else j--;
+        }
+        return ans;
+    }
+};
+
+// Time Complexity : O(NlogN*(2*N)) => O(NlogN);
+// Space Complexity: O(N);
