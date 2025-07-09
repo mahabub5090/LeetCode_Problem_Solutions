07819808fa56e43059dd94fef5f3a0a74231659a
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-07-09 23:37:34 +0600

    Day_09_July_2025

--- Medium/3439. Reschedule Meetings for Maximum Free Time I.cpp
+++ Medium/3439. Reschedule Meetings for Maximum Free Time I.cpp
@@ -1,4 +1,5 @@
 // Problem Link: https://leetcode.com/problems/reschedule-meetings-for-maximum-free-time-i/description/
+// Problem Link: https://leetcode.com/problems/reschedule-meetings-for-maximum-free-time-i/description/?envType=daily-question&envId=2025-07-09
 
 class Solution {
 public:
@@ -33,3 +34,29 @@ public:
 
 // Time Complexity : O(3*N) => O(N);
 // Space Complexity: O(2*N) => O(N);
+
+// Way: 2 =>
+
+class Solution {
+public:
+    int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
+       
+        int ans=0,len=0,n=startTime.size();
+
+        for(int i=0;i<n;i++){
+            len+=endTime[i]-startTime[i];
+
+            int l=(i<k?0:endTime[i-k]);
+            int r=(i==n-1?eventTime:startTime[i+1]);
+            ans=max(ans,r-l-len);
+
+            if(i+1>=k){
+                len-=endTime[i-k+1]-startTime[i-k+1];
+            }
+        }
+        return ans;
+    }
+};
+
+// Time Complexity : O(N);
+// Space Complexity: O(1);
