caec842e2f1fb8da0c820956ff99cac9b05a4eae
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-07-21 23:52:22 +0600

    Day_21_July_2025

--- Easy/1957. Delete Characters to Make Fancy String.cpp
+++ Easy/1957. Delete Characters to Make Fancy String.cpp
@@ -1,4 +1,5 @@
 // Problem Link: https://leetcode.com/problems/delete-characters-to-make-fancy-string/description/
+// Problem Link: https://leetcode.com/problems/delete-characters-to-make-fancy-string/description/?envType=daily-question&envId=2025-07-21
 
 class Solution {
 public:
@@ -18,6 +19,23 @@ public:
             }
         }
         return fancy;
+    }
+};
+
+// Time Complexity : O(N);
+// Space Complexity: O(1);
+
+// Way: 2 =>
+
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string ans="";
+        for(auto &c:s){
+            if(ans.size()>=2 && c==ans.back() && c==ans[ans.size()-2])continue;
+            else ans+=c;
+        }
+        return ans;
     }
 };
 
