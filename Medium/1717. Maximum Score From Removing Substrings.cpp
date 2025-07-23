c76978c324450547e27bf03c1c9d55fcaea951f6
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-07-23 22:57:29 +0600

    Day_23_July_2025

--- Medium/1717. Maximum Score From Removing Substrings.cpp
+++ Medium/1717. Maximum Score From Removing Substrings.cpp
@@ -1,4 +1,5 @@
 // Problem Link: https://leetcode.com/problems/maximum-score-from-removing-substrings/?envType=daily-question&envId=2024-07-12
+// Problem Link: https://leetcode.com/problems/maximum-score-from-removing-substrings/description/?envType=daily-question&envId=2025-07-23
 
 class Solution {
 public:
@@ -35,6 +36,34 @@ public:
 // Time Complexity : O(3*N) => O(N);
 // Space Complexity: O(2*N+1) => O(N);
 
+// Way: 2 =>
 
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        if(x<y){
+            swap(x,y);
+            reverse(s.begin(),s.end());
+        }
+
+        int a=0,b=0,cnt=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='a')a++;
+            else if(s[i]=='b'){
+                if(a>0)a--,cnt+=x;
+                else b++;
+            }
+            else{
+                cnt+=min(a,b)*y;
+                a=0,b=0;
+            }
+        }
+        cnt+=min(a,b)*y;
+        return cnt;
+    }
+};
+
+// Time Complexity : O(N);
+// Space Complexity: O(1);
 
 
