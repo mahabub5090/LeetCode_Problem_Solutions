182fece77ed1f681e97593f34f6c31eba34e0ff9
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-07-29 22:20:44 +0600

    Solution added of Daily problem 'Day_29_July_2025'

--- Medium/2411. Smallest Subarrays With Maximum Bitwise OR.cpp
+++ Medium/2411. Smallest Subarrays With Maximum Bitwise OR.cpp
@@ -1,2 +1,21 @@
 // Problem Link: http://leetcode.com/problems/smallest-subarrays-with-maximum-bitwise-or/editorial/?envType=daily-question&envId=2025-07-29
 
+class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>pos(32,-1),ans(n);
+        for(int i=n-1;i>=0;i--){
+            int j=i;
+            for(int bit=0;bit<32;bit++){
+                if(nums[i]&(1<<bit))pos[bit]=i;
+                else if(pos[bit]!=-1)j=max(j,pos[bit]);
+            }
+            ans[i]=j-i+1;
+        }
+        return ans;
+    }
+};
+
+// Time Complexity : O(N*32) => O(N);
+// Space Complexity: O(32) => O(1);
