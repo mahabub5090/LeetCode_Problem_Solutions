b47fe5b0076cd239f9306824b5f0ecd173f052d5
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-07-30 23:54:06 +0600

    Day_30_July_2025

--- Medium/2419. Longest Subarray With Maximum Bitwise AND.cpp
+++ Medium/2419. Longest Subarray With Maximum Bitwise AND.cpp
@@ -1,4 +1,5 @@
 // Problem Link: https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/description/?envType=daily-question&envId=2024-09-14
+// Problem Link: https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/description/?envType=daily-question&envId=2025-07-30
 
 class Solution {
 public:
@@ -15,4 +16,23 @@ public:
 };
 
 // Time Complexity : O(N);
+// Space Complexity: O(1);
+
+// Way: 2 =>
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxAnd=0;for(auto &c:nums)maxAnd=max(maxAnd,c);
+        int ans=0,cnt=0;
+        for(auto &c:nums){
+            if(c==maxAnd)cnt++;
+            else ans=max(ans,cnt),cnt=0;
+        }
+        ans=max(ans,cnt);
+        return ans;
+    }
+};
+
+// Time Complexity : O(2*N) => O(N);
 // Space Complexity: O(1);
