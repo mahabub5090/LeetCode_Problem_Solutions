5f319598a09ec1c1e09c4cd496fc3b3aee8eb6e7
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-08-16 22:22:57 +0600

    Solution added of Daily problem 'Day_16_August_2025'

--- Easy/1323. Maximum 69 Number.cpp
+++ Easy/1323. Maximum 69 Number.cpp
@@ -1,2 +1,18 @@
 //  Problem Link: https://leetcode.com/problems/maximum-69-number/description/?envType=daily-question&envId=2025-08-16
 
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s=to_string(num);
+        for(auto &c:s){
+            if(c=='6'){
+                c='9';
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
+
+// Time Complexity : O(maximam number of digits in num)=> O(5) => O(1);
+// Space Complexity: O(1);
