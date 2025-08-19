3aff25fac37f3aa3f92283370c3b24353229332b
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-08-20 00:08:47 +0600

    Solution added of Daily problem 'Day_19_August_2025'

--- Medium/2348. Number of Zero-Filled Subarrays.cpp
+++ Medium/2348. Number of Zero-Filled Subarrays.cpp
@@ -1,2 +1,20 @@
 // Problem Link: https://leetcode.com/problems/number-of-zero-filled-subarrays/description/?envType=daily-question&envId=2025-08-19
 
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long cnt=0;
+        int i=0,j=0,n=nums.size();
+        int index=-1;
+        while(j<n){
+            if(nums[j])index=j;
+            j++;
+            if(index==-1)cnt+=j-i;
+            else i=j,index=-1;
+        }
+        return cnt;
+    }
+};
+
+// Time Complexity : O(N);
+// Space Complexity: O(1);
