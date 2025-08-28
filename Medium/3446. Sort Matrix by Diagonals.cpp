26a2e9f7f566f376fd18b244b4b0d41f1d9891c5
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-08-28 23:26:13 +0600

    Day_28_August_2025

--- Medium/3446. Sort Matrix by Diagonals.cpp
+++ Medium/3446. Sort Matrix by Diagonals.cpp
@@ -1,44 +1,45 @@
 // Problem Link: https://leetcode.com/problems/sort-matrix-by-diagonals/description/
+// Problem Link: https://leetcode.com/problems/sort-matrix-by-diagonals/description/?envType=daily-question&envId=2025-08-28
+
 
 class Solution {
-    public:
-        vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
-            int n=grid.size();
-            for(int i=0;i<n;i++){
-                vector<int>vc;
-                int j=i,k=0;
-                while(j<n && k<n){
-                    vc.push_back(grid[j][k]);
-                    j++,k++;
-                }
-                sort(vc.begin(),vc.end());
-                j=i,k=0;
-                while(j<n && k<n){
-                    grid[j][k]=vc.back();
-                    vc.pop_back();
-                    j++,k++;
-                }
-    
-                if(i==0)continue;
-    
-                vc.clear();
-                j=0,k=i;
-                while(j<n && k<n){
-                    vc.push_back(grid[j][k]);
-                    j++,k++;
-                }
-                sort(vc.rbegin(),vc.rend());
-                j=0,k=i;
-                while(j<n && k<n){
-                    grid[j][k]=vc.back();
-                    vc.pop_back();
-                    j++,k++;
-                }
-                
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        for(int i=0;i<n;i++){
+            vector<int>vc;
+            
+            int j=i,k=0;
+            while(j<n && k<n){
+                vc.push_back(grid[j][k]);
+                j++,k++;
             }
-            return grid;
+            sort(vc.begin(),vc.end());
+            j=i,k=0;
+            while(j<n && k<n){
+                grid[j][k]=vc.back();
+                vc.pop_back();
+                j++,k++;
+            }
+
+            if(i==0)continue;
+
+            j=0,k=i;
+            while(j<n && k<n){
+                vc.push_back(grid[j][k]);
+                j++,k++;
+            }
+            sort(vc.rbegin(),vc.rend());
+            j=0,k=i;
+            while(j<n && k<n){
+                grid[j][k]=vc.back();
+                vc.pop_back();
+                j++,k++;
+            }    
         }
-    };
-    
-    // Time Complexity : O(N*N) => O(N^2);
-    // Space Complexity: O(N*N) => O(N^2);
+        return grid;
+    }
+};
+
+// Time Complexity : O(N*N*logN) => O((N^2)*logN);
+// Space Complexity: O(N);
