c363f9c44f32da3d06c114ee7a5679c1b400adf6
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-08-29 22:46:46 +0600

    Day_29_August_2025

---
+++ Medium/3021. Alice and Bob Playing Flower Game.cpp
@@ -0,0 +1,11 @@
+// Problem Link: https://leetcode.com/problems/alice-and-bob-playing-flower-game/description/?envType=daily-question&envId=2025-08-29
+
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+       return (1ll*m*n)/2; 
+    }
+};
+
+// Time Complexity : O(1);
+// Space Complexity: O(1);
