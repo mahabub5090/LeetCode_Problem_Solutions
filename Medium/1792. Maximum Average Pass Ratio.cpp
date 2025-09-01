c6db967c85971df928234aca65e7754bb00fe94a
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-09-01 23:27:59 +0600

    Day_01_September_2025

--- Medium/1792. Maximum Average Pass Ratio.cpp
+++ Medium/1792. Maximum Average Pass Ratio.cpp
@@ -1,4 +1,6 @@
 // Problem Link: https://leetcode.com/problems/maximum-average-pass-ratio/?envType=daily-question&envId=2024-12-15
+// Problem Link: https://leetcode.com/problems/maximum-average-pass-ratio/description/?envType=daily-question&envId=2025-09-01
+
 
 class Solution {
 public:
@@ -28,6 +30,36 @@ public:
             ans+=1.0*get<1>(pq.top())/get<2>(pq.top());
             pq.pop();
         }
+        ans/=classes.size();
+        return ans;
+    }
+};
+
+// Time Complexity : O(NlogN) + O(KlogN) => O(NlogN);
+// Space Complexity: O(N);
+// K is the number of classes;
+
+// Way: 2 =>
+
+class Solution {
+public:
+    double gain(int a,int b){
+        return ((a+1)/((double)(b+1)))-(a/(double)b);
+    }
+    static bool cmp(auto &a,auto &b){
+        return a[0]<b[0];
+    }
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        priority_queue<vector<double>, vector<vector<double>>,bool(*)(vector<double>&,vector<double>&)>pq(cmp);
+
+        for(auto &c:classes)pq.push({gain(c[0],c[1]),(double)c[0],(double)c[1]});
+
+        while(extraStudents--){
+            auto c=pq.top();pq.pop();
+            pq.push({gain(c[1]+1,c[2]+1),c[1]+1,c[2]+1});
+        }
+        double ans=0;
+        while(pq.size())ans+=pq.top()[1]/pq.top()[2],pq.pop();
         ans/=classes.size();
         return ans;
     }
