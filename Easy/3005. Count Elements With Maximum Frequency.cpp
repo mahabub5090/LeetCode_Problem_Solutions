7e0b783f99732391d1fd23275cea13b29de59821
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-09-23 00:20:51 +0600

    Solution added of Daily problem 'Day_22_September_2025'

--- Easy/3005. Count Elements With Maximum Frequency.cpp
+++ Easy/3005. Count Elements With Maximum Frequency.cpp
@@ -1,2 +1,15 @@
 // Problem Link: https://leetcode.com/problems/count-elements-with-maximum-frequency/description/?envType=daily-question&envId=2025-09-22
 
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int,int>mp;int mx=0;
+        for(auto &c:nums)mp[c]++,mx=max(mx,mp[c]);
+        int ans=0;
+        for(auto &c:mp)if(c.second==mx)ans+=mx;
+        return ans;
+    }
+};
+
+// Time Complexity : O(N);
+// Space Complexity: O(N);
