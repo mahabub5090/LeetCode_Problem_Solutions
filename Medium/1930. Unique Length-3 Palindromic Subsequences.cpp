e5cc7614987e0ac34c565275464d9ff1df77c8f7
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-11-21 21:57:32 +0600

    Optimised Space Complexity for the problem '1930. Unique Length-3 Palindromic Subsequences'

--- Medium/1930. Unique Length-3 Palindromic Subsequences.cpp
+++ Medium/1930. Unique Length-3 Palindromic Subsequences.cpp
@@ -29,24 +29,48 @@
 
 // Way: 2 =>
 
+// class Solution {
+// public:
+//     int countPalindromicSubsequence(string s) {
+//         int ans=0,n=s.size();
+//         unordered_map<char,vector<int>>mp;
+//         for(int i=0;i<n;i++)mp[s[i]].push_back(i);
+//         for(auto &[cc,vc]:mp){
+//             int len=vc.size();
+//             if(len==1)continue;
+//             int l=vc[0],r=vc[len-1];
+//             unordered_map<char,int>cnt;
+//             for(int i=l;i<=r;i++)cnt[s[i]]++;
+//             ans+=cnt.size()-(cnt.count(cc) && cnt[cc]<3);
+//             // ans+=cnt.size()-(len==2); // same as previous line;
+//         }
+//         return ans;
+//     }
+// };
+
+// // Time Complexity : O(26*N) => O(N);
+// // Space Complexity: O(N);
+
+// Way: 3 =>
+
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
         int ans=0,n=s.size();
-        unordered_map<char,vector<int>>mp;
-        for(int i=0;i<n;i++)mp[s[i]].push_back(i);
-        for(auto &[cc,vc]:mp){
-            int len=vc.size();
-            if(len==1)continue;
-            int l=vc[0],r=vc[len-1];
+        unordered_map<char,pair<int,int>>mp;
+        for(int i=0;i<n;i++){
+            if(mp.count(s[i])==0)mp[s[i]].first=i;
+            mp[s[i]].second=i;
+        }
+        for(auto &[cc,interval]:mp){
+            auto &[l,r]=interval;
             unordered_map<char,int>cnt;
             for(int i=l;i<=r;i++)cnt[s[i]]++;
             ans+=cnt.size()-(cnt.count(cc) && cnt[cc]<3);
-            // ans+=cnt.size()-(len==2); // same as previous line;
         }
         return ans;
     }
 };
 
 // Time Complexity : O(26*N) => O(N);
-// Space Complexity: O(N);
+// Space Complexity: O(26*26) => O(1);
