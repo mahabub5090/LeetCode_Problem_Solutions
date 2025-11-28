6c4a8d08bb12b0e9691898549d30aba32a119839
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-11-28 22:54:42 +0600

    Day_28_November_2025

--- Hard/2872. Maximum Number of K-Divisible Components.cpp
+++ Hard/2872. Maximum Number of K-Divisible Components.cpp
@@ -1,4 +1,5 @@
 // Problem Link: https://leetcode.com/problems/maximum-number-of-k-divisible-components/description/?envType=daily-question&envId=2024-12-21
+// Problem Link: https://leetcode.com/problems/maximum-number-of-k-divisible-components/description/?envType=daily-question&envId=2025-11-28
 
 class Solution {
 public:
