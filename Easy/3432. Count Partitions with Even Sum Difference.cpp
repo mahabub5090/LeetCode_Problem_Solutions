6b6c9f4dab74dc759c51947319e604f84268a68f
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-12-05 22:35:05 +0600

    Solution updated of the problem '3432. Count Partitions with Even Sum Difference'

--- Easy/3432. Count Partitions with Even Sum Difference.cpp
+++ Easy/3432. Count Partitions with Even Sum Difference.cpp
@@ -1,18 +1,31 @@
 // Problem Link: https://leetcode.com/problems/count-partitions-with-even-sum-difference/description/?envType=daily-question&envId=2025-12-05
 
+// class Solution {
+// public:
+//     int countPartitions(vector<int>& nums) {
+//         int tt=accumulate(nums.begin(),nums.end(),0);
+//         int sum=0,ans=0;
+//         for(int i=0;i<nums.size()-1;i++){
+//             tt-=nums[i];
+//             sum+=nums[i];
+//             ans+=abs(tt-sum)%2==0;
+//         }
+//         return ans;
+//     }
+// };
+
+// // Time Complexity : O(2*N) => O(N);
+// // Space Complexity: O(1);
+
+// Way: 2 =>
+
 class Solution {
 public:
     int countPartitions(vector<int>& nums) {
         int tt=accumulate(nums.begin(),nums.end(),0);
-        int sum=0,ans=0;
-        for(int i=0;i<nums.size()-1;i++){
-            tt-=nums[i];
-            sum+=nums[i];
-            ans+=abs(tt-sum)%2==0;
-        }
-        return ans;
+        return tt%2?0:nums.size()-1;
     }
 };
 
-// Time Complexity : O(2*N) => O(N);
+// Time Complexity : O(N);
 // Space Complexity: O(1);
