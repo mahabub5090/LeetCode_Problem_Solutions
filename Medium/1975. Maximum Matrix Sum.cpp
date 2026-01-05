da5446d8e4599fb8da9b0785ab536ccba110a0a3
mahabub5090 <mdmahabub.rahman789@gmail.com>
2026-01-05 21:17:31 +0600

    Day_05_January_2026

--- Medium/1975. Maximum Matrix Sum.cpp
+++ Medium/1975. Maximum Matrix Sum.cpp
@@ -1,21 +1,44 @@
 // Problem Link: https://leetcode.com/problems/maximum-matrix-sum/submissions/1461400347/?envType=daily-question&envId=2024-11-24
+// Problem Link: https://leetcode.com/problems/maximum-matrix-sum/description/?envType=daily-question&envId=2026-01-05
+
+// class Solution {
+// public:
+//     long long maxMatrixSum(vector<vector<int>>& matrix) {
+//         int neg=0,zero=0,mn=1e9;
+//         long long ans=0;
+//         for(auto &c:matrix){
+//             for(auto &cc:c){
+//                 if(cc==0)zero++;
+//                 else if(cc<0)neg++;
+//                 mn=min(mn,abs(cc));
+//             }
+//         }
+//         if(neg%2==0 || (neg%2 && zero)){
+//             return ans;
+//         }
+//         return ans-abs(2*mn);
+//     }
+// };
+
+// // Time Complexity : O(N*M) => O(N^2);
+// // Space Complexity: O(1);
+
+// Way: 2 =>
 
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
-        int neg=0,zero=0,mn=1e9;
-        long long ans=0;
+        long long sum=0,zero=0,neg=0,mn=INT_MAX;
         for(auto &c:matrix){
             for(auto &cc:c){
                 if(cc==0)zero++;
-                else if(cc<0)neg++;
-                mn=min(mn,abs(cc));
+                if(cc<0)neg++;
+                sum+=abs(cc);
+                mn=min(mn,abs(1ll*cc));
             }
         }
-        if(neg%2==0 || (neg%2 && zero)){
-            return ans;
-        }
-        return ans-abs(2*mn);
+        if(zero==0 && neg%2)sum-=2*mn;
+        return sum;
     }
 };
 
