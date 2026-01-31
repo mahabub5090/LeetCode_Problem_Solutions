479163e79e111ee5fe60c6c1aa3a6a72fa3320f2
mahabub5090 <mdmahabub.rahman789@gmail.com>
2026-01-31 18:01:25 +0600

    Solution added of the problem '744. Find Smallest Letter Greater Than Target.cpp'

--- Easy/744. Find Smallest Letter Greater Than Target.cpp
+++ Easy/744. Find Smallest Letter Greater Than Target.cpp
@@ -1,1 +1,14 @@
 // Problem Link: https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/?envType=daily-question&envId=2026-01-31
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for(auto &c:letters){
+            if(c>target)return c;
+        }
+        return letters[0];
+    }
+};
+
+// Time Complexity : O(N);
+// Space Complexity: O(1);
