a625430b70a775cf67e735441aa62dfcf7105eb5
mahabub5090 <mdmahabub.rahman789@gmail.com>
2026-02-08 19:34:51 +0600

    Day_08_February_2026

---
+++ Easy/110. Balanced Binary Tree.cpp
@@ -0,0 +1,30 @@
+// Problem Link: https://leetcode.com/problems/balanced-binary-tree/description/?envType=daily-question&envId=2026-02-08
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(auto &root){
+        if(root==NULL)return 0;
+        int l=dfs(root->left);
+        int r=dfs(root->right);
+        if(abs(l-r)>1)return -1e9/2;
+        return 1+max(l,r);
+    }
+   
+    bool isBalanced(TreeNode* root) {
+        return root?(dfs(root)>0):1;
+    }
+};
+
+// Time Complexity : O(N);
+// Space Complexity: O(maxDept) => O(N); 
