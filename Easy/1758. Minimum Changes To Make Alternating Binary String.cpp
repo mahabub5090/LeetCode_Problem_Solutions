23738637ba207fa0f6b024495f0bf0be15e26c4f
mahabub5090 <mdmahabub.rahman789@gmail.com>
2026-03-05 19:53:29 +0600

    Day_05_March_2026

---
+++ Easy/1758. Minimum Changes To Make Alternating Binary String.cpp
@@ -0,0 +1,19 @@
+// Problem Link: https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/description/?envType=daily-question&envId=2026-03-05
+
+class Solution {
+public:
+    int minOperations(string s) {
+        int ans=0,ans2=0;
+        char curr='0',curr2='1';
+        for(auto &c:s){
+            if(c!=curr)ans++;
+            if(c!=curr2)ans2++;
+            curr='0'+(curr=='0');
+            curr2='0'+(curr2=='0');
+        }
+        return min(ans,ans2);
+    }
+};
+
+// Time Complexity : O(N);
+// Space Complexity: O(1);
