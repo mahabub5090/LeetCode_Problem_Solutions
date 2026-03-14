ea5acad97dfdac9ae0e27841c577b2662a9b29dd
mahabub5090 <mdmahabub.rahman789@gmail.com>
2026-03-14 15:33:26 +0600

    Day_14_March_2026

--- Medium/1415. The k-th Lexicographical String of All Happy Strings of Length n.cpp
+++ Medium/1415. The k-th Lexicographical String of All Happy Strings of Length n.cpp
@@ -1,25 +1,56 @@
 // Problem Link: https://leetcode.com/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/description/?envType=daily-question&envId=2025-02-19
 
+// class Solution {
+// public:
+//     void all(string s,int n,auto &happy){
+//         if(s.size()==n){
+//             happy.push_back(s);
+//             return;
+//         };
+//         for(auto &c:{'a','b','c'}){
+//             if(s.empty() || s.back()!=c){
+//                 all(s+c,n,happy);
+//             }
+//         }
+//     }
+//     string getHappyString(int n, int k) {
+//         vector<string>happy;
+//         all("",n,happy);
+//         if(k>happy.size())return "";
+//         return happy[k-1];
+//     }
+// };
+
+// // Time Complexity : O(N*(2^N));
+// // Space Complexity: O(N*(2^N));
+
+// way: 2 =>
+
 class Solution {
 public:
-    void all(string s,int n,auto &happy){
+    string genarate(string s,int n,int &cnt,int k){
         if(s.size()==n){
-            happy.push_back(s);
-            return;
-        };
-        for(auto &c:{'a','b','c'}){
+            cnt++;
+            if(cnt==k)return s;
+            return "";
+        }
+
+        for(auto c:{'a','b','c'}){
             if(s.empty() || s.back()!=c){
-                all(s+c,n,happy);
+                string ans=genarate(s+c,n,cnt,k);
+                if(ans!="")return ans;
             }
         }
+        return "";
     }
+
     string getHappyString(int n, int k) {
-        vector<string>happy;
-        all("",n,happy);
-        if(k>happy.size())return "";
-        return happy[k-1];
+        int tt=3*(1<<(n-1));
+        if(k>tt)return "";
+        int cnt=0;
+        return genarate("",n,cnt,k);
     }
 };
 
-// Time Complexity : O(3^N);
-// Space Complexity: O(3^N);
+// Time Complexity : O(K*N) => O(N*(2^N));
+// Space Complexity: O(N);
