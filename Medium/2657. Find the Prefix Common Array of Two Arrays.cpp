a6e1fe5d5f4cb3aa98a3e61a06355dca1d09ffe6
mahabub5090 <mdmahabub.rahman789@gmail.com>
2026-05-20 21:28:00 +0600

    Day_20_May_2026

--- Medium/2657. Find the Prefix Common Array of Two Arrays.cpp
+++ Medium/2657. Find the Prefix Common Array of Two Arrays.cpp
@@ -1,20 +1,45 @@
 // Problem Link: https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/description/?envType=daily-question&envId=2025-01-14
+// Problem Link: https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/description/?envType=daily-question&envId=2026-05-20
+
+// class Solution {
+// public:
+//     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+//         int n=A.size(),cnt=0;
+//         vector<int>a(n,0),b(n,0),ans;
+//         for(int i=0;i<n;i++){
+//             if(b[A[i]-1])cnt++;
+//             a[A[i]-1]++;
+//             if(a[B[i]-1])cnt++;
+//             b[B[i]-1]++;
+//             ans.push_back(cnt);
+//         }
+//         return ans;
+//     }
+// };
+
+// // Time Complexity : O(N);
+// // Space Complexity: O(N);
+
+// Way: 2 =>
 
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        int n=A.size(),cnt=0;
-        vector<int>a(n,0),b(n,0),ans;
+        int n=A.size();
+        unordered_map<int,int> mp;
+        vector<int> ans(n,0);
+        
+        int cnt=0;
         for(int i=0;i<n;i++){
-            if(b[A[i]-1])cnt++;
-            a[A[i]-1]++;
-            if(a[B[i]-1])cnt++;
-            b[B[i]-1]++;
-            ans.push_back(cnt);
+            mp[A[i]]++,mp[B[i]]++;
+            if(mp[A[i]]==2) cnt++;
+            if(A[i]!=B[i] && mp[B[i]]==2) cnt++;
+            
+            ans[i]=cnt;
         }
         return ans;
     }
 };
 
 // Time Complexity : O(N);
-// Space Complexity: O(N);
+// Space Complexity: O(N) + O(N) => O(N);
