36da86a7f5525b07e62334c395d09cde7d3e9bc2
mahabub5090 <mdmahabub.rahman789@gmail.com>
2026-05-21 22:22:09 +0600

    Day_21_May_2026

--- Medium/3043. Find the Length of the Longest Common Prefix.cpp
+++ Medium/3043. Find the Length of the Longest Common Prefix.cpp
@@ -1,29 +1,28 @@
 // Problem Link: https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/description/?envType=daily-question&envId=2024-09-24
+// Problem Link: https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/description/?envType=daily-question&envId=2026-05-21
 
 class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        map<string,int>mp;
-        for(auto &c:arr1){
-            string s=to_string(c);
-            string cmp="";
-            for(int i=0;i<s.size();i++){
-                cmp+=s[i];
-                mp[cmp]++;
+        unordered_map<string,int> mp;
+        for(auto &c:arr1){    
+            string a=to_string(c), pref="";
+            for(auto &c2:a)pref+=c2, mp[pref]++;
+        }
+
+        int ans=0;
+        for(auto &c:arr2){    
+            string a=to_string(c), pref="";
+            for(auto &c2:a){
+                pref+=c2;
+                if(mp.count(pref)) ans=max(ans,(int)pref.size());
             }
         }
-        int cnt=0;
-        for(auto &c:arr2){
-            string s=to_string(c);
-            string cmp="";
-            for(int i=0;i<s.size();i++){
-                cmp+=s[i];
-                if(mp[cmp])cnt=max(cnt,i+1);
-            }
-        }
-        return cnt;
+        return ans;
     }
 };
 
-// Time Complexity : O(8*N);
-// Space Complexity: O(8*N);
+// Time Complexity : O(N*(L*L)) + O(M*(L*L)) => O((N+M)*(9*9)) => O(N+M); 
+// Space Complexity: O(N*(L*L)) => O(N*(9*9)) => O(N);
+// L = maximum possible number of digits in arr1[i] or arr2[i];
+// 1 <= arr1[i], arr2[i] <= 1e8;
