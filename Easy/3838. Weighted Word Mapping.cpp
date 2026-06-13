873165b985e147c8c1dc283263ebbfcc07b29d1a
mahabub5090 <mdmahabub.rahman789@gmail.com>
2026-06-13 22:34:54 +0600

    Day_13_June_2026

---
+++ Easy/3838. Weighted Word Mapping.cpp
@@ -0,0 +1,20 @@
+// Problem Link: https://leetcode.com/problems/weighted-word-mapping/description/?envType=daily-question&envId=2026-06-13
+
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans="";
+        for(auto &word:words){
+            int cnt=0;
+            for(auto &c:word) cnt+=weights[c-'a'];
+            cnt%=26;
+            ans+=(char)'a'+(25-cnt);
+        }
+        return ans;
+    }
+};
+
+// Time Complexity : O(N*M) => O(N^2);
+// Space Complexity: O(1);
+// N=words.size(), M=words[i].size();
+ 
