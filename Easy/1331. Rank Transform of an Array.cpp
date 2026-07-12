153db602be75a8be93d83daf74a62a827b453e27
mahabub5090 <mdmahabub.rahman789@gmail.com>
2026-07-12 22:14:43 +0600

    Day_12_July_2026

--- Easy/1331. Rank Transform of an Array.cpp
+++ Easy/1331. Rank Transform of an Array.cpp
@@ -1,19 +1,18 @@
 // Problem Link: https://leetcode.com/problems/rank-transform-of-an-array/submissions/1409212335/?envType=daily-question&envId=2024-10-02
+// Problem Link: https://leetcode.com/problems/rank-transform-of-an-array/description/?envType=daily-question&envId=2026-07-12
 
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        set<int>st;for(auto &c:arr)st.insert(c);
-        map<int,int>mp;
-        int j=1;
-        for(auto &c:st){
-            mp[c]=j++;
-        }
-        vector<int>ans;
-        for(auto &c:arr)ans.push_back(mp[c]);
+        set<int> st(arr.begin(),arr.end());
+        unordered_map<int,int> mp;
+        int cnt=1;
+        for(auto &c:st) mp[c]=cnt++;
+        vector<int> ans;
+        for(auto &c:arr) ans.push_back(mp[c]);
         return ans;
     }
 };
 
-// Time Complexity : O(3*N) => O(N);
-// Space Complexity: O(3*N) => O(N);
+// Time Complexity : O(NlogN) + O(N) + O(N) => O(NlogN);
+// Space Complexity: O(N) + O(N) + O(N) => O(N);
