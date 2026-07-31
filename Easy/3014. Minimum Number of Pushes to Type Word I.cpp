3829f37584c54fbd9d6bcf0bbdc849b863acce81
mahabub5090 <mdmahabub.rahman789@gmail.com>
2026-07-31 22:47:50 +0600

    Day_31_July_2026

--- Easy/3014. Minimum Number of Pushes to Type Word I.cpp
+++ Easy/3014. Minimum Number of Pushes to Type Word I.cpp
@@ -1,17 +1,33 @@
 // Problem Link: https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i/description/?envType=daily-question&envId=2026-07-30
 
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
+//         int push=0, curr=1, ans=0;
+//         for(auto &c:word){
+//             push++;
+//             if(push>8) curr++, push%=8;
+//             ans+=curr;
+//         }
+//         return ans;
+//     }
+// };
+
+// // Time Complexity : O(N);
+// // Space Complexity: O(1);
+
+
+// Way: 2 =>
+
 class Solution {
 public:
     int minimumPushes(string word) {
-        int push=0, curr=1, ans=0;
-        for(auto &c:word){
-            push++;
-            if(push>8) curr++, push%=8;
-            ans+=curr;
-        }
+        int n=word.size();
+        int mx=n/8, rem=n%8;
+        int ans=8*(mx*(mx+1)/2) + (rem*(mx+1));
         return ans;
     }
 };
 
-// Time Complexity : O(N);
+// Time Complexity : O(1);
 // Space Complexity: O(1);
--- Medium/3016. Minimum Number of Pushes to Type Word II.cpp
+++ Medium/3016. Minimum Number of Pushes to Type Word II.cpp
@@ -1,18 +1,44 @@
 // Problem Link: https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/description/?envType=daily-question&envId=2024-08-06
+// Problem Link: https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/description/?envType=daily-question&envId=2026-07-31
+
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
+//         vector<int>vc(26);
+//         for(auto &c:word)vc[c-'a']++;
+//         sort(vc.begin(),vc.end(),greater<int>());
+//         int ans=0,cnt=0;
+//         for(auto &c:vc){
+//             ans+=c*(1+(cnt++/8));
+//         }
+//         return ans;   
+//     }
+// };
+
+// // Time Complexity : O(N+log26+26) => O(N);
+// // Space Complexity: O(26) => O(1); 
+
+
+// way: 2 =>
 
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int>vc(26);
-        for(auto &c:word)vc[c-'a']++;
-        sort(vc.begin(),vc.end(),greater<int>());
-        int ans=0,cnt=0;
-        for(auto &c:vc){
-            ans+=c*(1+(cnt++/8));
+        vector<int> cnt(26,0);
+        for(auto &c:word) cnt[c-'a']++;
+        sort(cnt.rbegin(),cnt.rend());
+
+        int push=0, curr=1, ans=0;
+        for(auto &c:cnt){
+            if(c==0) break;
+
+            push++;
+            if(push>8) curr++, push%=8;
+            ans+=curr*c;
         }
-        return ans;   
+        return ans;
     }
 };
 
-// Time Complexity : O(2*N+NlogN) => O(NlogN);
-// Space Complexity: O(N);
+// Time Complexity : O(N) + O(log26) + O(26) => O(N);
+// Space Complexity: O(26) => O(1);
