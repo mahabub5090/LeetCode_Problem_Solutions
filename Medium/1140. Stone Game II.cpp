ba3a6ff56a1f7c2a0f0f23c1e1cca7e68ef8edab
mahabub5090 <mdmahabub.rahman789@gmail.com>
2026-08-09 23:55:11 +0600

    Day_09_August_2026

--- Medium/1140. Stone Game II.cpp
+++ Medium/1140. Stone Game II.cpp
@@ -1,32 +1,33 @@
 // Problem Link: https://leetcode.com/problems/stone-game-ii/description/?envType=daily-question&envId=2024-08-20
+// Problem Link: https://leetcode.com/problems/stone-game-ii/description/?envType=daily-question&envId=2026-08-09
 
 class Solution {
 public:
     int dp[2][101][101];
+    int dfs(int turn,int i,int m,auto &vc){
+        int n=vc.size();
+        if(i==n) return 0;
+        if(dp[turn][i][m]!=-1)return dp[turn][i][m];
 
-    int dfs(char c,int i,int m,auto &vc){
-        if(i==vc.size()) return 0;
-        if(dp[c-'a'][i][m]!=-1)return dp[c-'a'][i][m];
-
-        int ans=(c=='a'?0:1e9);
+        int ans=(turn==0?0:INT_MAX);
         int tt=0;
-        for(int j=0;j<2*m;j++){
-            if(i+j==vc.size())break;
-            tt+=vc[i+j];
-            if(c=='a'){
-                ans=max(ans,tt+dfs('b',i+j+1,max(j+1,m),vc));
-            }
-            else ans=min(ans,dfs('a',i+j+1,max(j+1,m),vc));
+        for(int j=1;j<=2*m && i+j<=n;j++){
+            tt+=vc[i+j-1];
+            int rest=dfs(!turn,i+j,max(j,m),vc);
+
+            if(turn==0) ans=max(ans,tt+rest);
+            else ans=min(ans,rest);
         }
-        return dp[c-'a'][i][m]=ans;
+        return dp[turn][i][m]=ans;
     }
+
 
     int stoneGameII(vector<int>& piles) {   
         memset(dp, -1, sizeof(dp));    
-        return dfs('a',0,1,piles);
+        return dfs(0,0,1,piles);
     }
 };
 
-// Time Complexity : O((101^2)*101) => O(100^3) => O(N^3); 
-// Space Complexity: O(2*101*101) + O(100) => 100^2 => O(N^2);
-// Max piles size is 100 (max N);
+// Time Complexity : O(2*(N^2)) + O(2*(N^2)*N) => O(N^3); 
+// Space Complexity: O(2*(N^2)) + O(N) => O(N^2); // O(N) for recursion stack;
+// N <= 100;
