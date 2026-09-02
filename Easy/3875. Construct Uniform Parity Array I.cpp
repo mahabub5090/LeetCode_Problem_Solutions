af64629d0a00412f41412b4a89824c2ef0711d9c
mahabub5090 <mdmahabub.rahman789@gmail.com>
2026-09-02 21:38:28 +0600

    Day_02_September_2026

---
+++ Easy/3875. Construct Uniform Parity Array I.cpp
@@ -0,0 +1,11 @@
+// Problem Link: https://leetcode.com/problems/construct-uniform-parity-array-i/description/?envType=daily-question&envId=2026-09-02
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return 1;
+    }
+};
+
+// Time Complexity : O(1);
+// Space Complexity: O(1);
